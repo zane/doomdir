33f306eb1b0153821554a99d6c6cc213ff5aa24b
Zane Shelby <zane@Zanes-MacBook-Pro.local>
2021-01-29 12:48:52 -0500

    Use package 'ov, which is still used -- woops!

--- package/.config/doom/packages.el
+++ package/.config/doom/packages.el
@@ -14,6 +14,7 @@
 (package! git-link)
 (package! lispy)
 (package! lispyville)
+(package! ov)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
